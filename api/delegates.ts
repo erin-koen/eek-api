11810daa43636fde562c299986dc7a1e43fa94fe
Erin Koen <46381469+erin-koen@users.noreply.github.com>
2023-08-01 17:54:53 -0400

    feat: add ethers (#1)
    * feat: add ethers for big numbahs
    
    * chore: general cleanup

--- api/delegates.ts
+++ api/delegates.ts
@@ -1,11 +1,13 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { gql, GraphQLClient } from 'graphql-request'
 import { loadEnv } from '../utils/loadEnv';
+import { formatUnits } from 'ethers';
 
-const endpoint = loadEnv('TALLY_URL')
-const apiKey = loadEnv('TALLY_API_KEY')
-const key = "Api-key"
-console.log({ endpoint })
+
+const tallyEndpoint = loadEnv('TALLY_URL')
+const tallyApiKey = loadEnv('TALLY_API_KEY')
+const tallyAuthKey = "Api-key"
+
 interface responseFormat {
 
   "governanceBySlug": {
@@ -26,44 +28,44 @@ interface responseFormat {
   }
 }
 
-
-
-const graphQLClient = new GraphQLClient(endpoint, {
+const graphQLClient = new GraphQLClient(tallyEndpoint, {
   headers: {
-    [key]: apiKey
+    [tallyAuthKey]: tallyApiKey
   }
 }
 )
+
+const query = gql`
+query {governanceBySlug(slug: "uniswap") {
+    delegates(sort: {field: VOTING_WEIGHT, order: DESC}, pagination: {limit: 100}) {
+        participation {
+            stats {
+                votingPower {
+                    net
+                }
+            }
+            account {
+                name
+                address
+            }
+        }
+    }
+}}
+`
+
 
 
 
 export default async (request: VercelRequest, response: VercelResponse) => {
 
-  const query = gql`
-    query {governanceBySlug(slug: "uniswap") {
-        delegates(sort: {field: VOTING_WEIGHT, order: DESC}, pagination: {limit: 100}) {
-            participation {
-                stats {
-                    votingPower {
-                        net
-                    }
-                }
-                account {
-                    name
-                    address
-                }
-            }
-        }
-    }}
-`
-
   const data = await graphQLClient.request<responseFormat>(query)
-  console.log(data)
-  const formattedData: { name: string, address: string, votes: string }[] = data.governanceBySlug.delegates.map((item) => {
+
+  const formattedData: { name: string, address: string, votes: number }[] = data.governanceBySlug.delegates.map((item) => {
+
     return {
       name: item.participation.account.name,
       address: item.participation.account.address,
-      votes: item.participation.stats.votingPower.net
+      votes: Number(formatUnits(item.participation.stats.votingPower.net, 18))
     }
 
   })
